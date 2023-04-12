019c86c9eb8595ee0e5fa9506e08e185ec21858b
Vaibhav Gupta <vaibhav.gupta1@prodo.in>
2023-04-12 13:55:38 +0530

    Live Updating Monitors

--- src/pages/user/components/MonitorTable.jsx
+++ src/pages/user/components/MonitorTable.jsx
@@ -24,9 +24,9 @@ const MonitorTable = () => {
 		useUpdateMonitorNameMutation();
 	const [logoutScreen, { isLoading: isLoggingOut }] =
 		useLogoutScreenMutation();
-	const { data: playlist } = useGetPlaylistsQuery();
-	const { data, isLoading } = useGetUserMonitorQuery();
-	const { data: groupedData } = useGetGroupedScreensQuery();
+	const { data: playlist } = useGetPlaylistsQuery({},{ pollingInterval: 1000, });
+	const { data, isLoading } = useGetUserMonitorQuery({},{ pollingInterval: 1000, });
+	const { data: groupedData } = useGetGroupedScreensQuery({},{ pollingInterval: 1000, });
 	const [messageApi, contextHolder] = message.useMessage();
 	const [showPass, setShowPass] = React.useState([]);
 	const [name, setName] = React.useState([]);
